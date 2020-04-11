0d66316d3c29b2212be91f51bc00652fb98952fb
vieyrapy <latinotanato@gmail.com>
2020-04-11 13:22:47 -0400

    Español

--- components/home.vue
+++ components/home.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="uk-section uk-section-primary">
       <div class="uk-container uk-text-center">
-        <h1 class="uk-margin-large-top">Ventas online</h1>
+        <h1 class="uk-margin-large-top">Bienvenidos</h1>
         <p class="uk-text-lead">Hoy mas que nunca estamos cerca tuyo. Nuestros vendedores están listos para atenderte</p>
       </div>
     </div>
@@ -10,7 +10,7 @@
       <div class="container">
         <div class="route-home uk-flex-center" uk-grid>
           <div hidden="true">
-            <router-link to="/agent" class="uk-button uk-button-primary uk-button-large">Join as Agent</router-link>
+            <router-link to="/agent" class="uk-button uk-button-primary uk-button-large">Vendedor</router-link>
           </div>
           <div>
             <router-link to="/caller" class="uk-button uk-button-primary uk-button-large">Llamar</router-link>
