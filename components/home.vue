b209492afae5ab8ed072cfb07426fc2b9e16f542
vieyrapy <latinotanato@gmail.com>
2020-04-13 16:32:29 -0400

    home

--- components/home.vue
+++ components/home.vue
@@ -4,8 +4,11 @@
 
       <div class="uk-width-1-1">
       <div class="uk-container uk-text-center">
+        
         <h1 class="uk-margin-large-top uk-text-bold">Sistema de ventas por videollamadas</h1>
-        <p class="uk-text-lead">Hoy mas que nunca estamos cerca tuyo. Nuestros vendedores están listos para atenderte</p>
+        <p class="uk-text-lead">Que la atención cara a cara nunca cambie.
+        Hoy más que nunca, tus clientes te quieren cerca. </p>
+        <p class="uk-text-lead"> #comprendesdecasa </p>
       
  
       <div class="container">
@@ -17,23 +20,22 @@
              <a href="/#/caller" class="uk-button uk-button-primary tm-button-primary uk-button-large tm-button-large ">Llamar Ahora!</a>
         </div>
 
-
         <div hidden="true">
           <router-link to="/agent" class="uk-button uk-button-primary uk-button-large">Agentes</router-link>
         </div>
       </div>
-     
 
         <p class="uk-section-small uk-width-large uk-align-center">
-           Sistema que emula un call center para atender a todos  tus clientes como si fuera que estan en tu local. Anímate a hacer un cambio en tu comercio y a vender diferente. 
+           Sistema que emula un call center para mejorar la atención de tus clientes.  Anímate a hacer un cambio en tu comercio y empezar a vender como siempre como si fuera que estan en tu local. 
         </p>
+
     </div>
 
 
       <div class="uk-section-small">
         <div class="uk-container uk-container-expand uk-text-center uk-position-relative">
           <ul uk-margin="" class="uk-subnav tm-subnav uk-flex-inline uk-flex-center uk-margin-remove-bottom">
-          <li class="uk-first-column">Versión 1.2.0</li> 
+         
           <li>
             <a href="/#/caracteristicas" class="uk-text-lowercase">Basic home</a>
           </li>
@@ -59,21 +61,21 @@
     <ul class="uk-slideshow-items">
         <li>
             <img src="assets/images/000.jpg" alt="" uk-cover>
-            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+            <div hidden="true" class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
                 <h3 class="uk-margin-remove">Bottom</h3>
                 <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
         </li>
         <li>
             <img src="assets/images/001.jpg" alt="" uk-cover>
-            <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+            <div hidden="true" class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
                 <h3 class="uk-margin-remove">Bottom</h3>
                 <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
         </li>
         <li>
             <img src="assets/images/002.jpg" alt="" uk-cover>
-            <div class="uk-overlay uk-overlay-primary uk-position-right uk-text-center uk-transition-slide-right uk-width-medium">
+            <div hidden="true" class="uk-overlay uk-overlay-primary uk-position-right uk-text-center uk-transition-slide-right uk-width-medium">
                 <h3 class="uk-margin-remove">Left</h3>
                 <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
